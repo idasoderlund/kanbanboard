f0c16ee881b49eed91756d11894b6e3a1d2b7d73
idasoderlund <soderlundida7711@gmail.com>
2025-08-31 12:23:08 +0200

    changed js to tsx

--- src/Contexts/TaskContext.tsx
+++ src/Contexts/TaskContext.tsx
@@ -1,35 +1,50 @@
-import React, { createContext, useState} from 'react';
+import React, { createContext, useState, type ReactNode} from 'react';
+import { Column, Task } from '.\Types.ts';
 
-export const TaskContext = createContext();
 
-const initialData = {
-    colums: [
+interface TaskContextTpe {
+    columns: Column[];
+    addTask: (ColumnId: string, task: Task) => void;
+    updateTask: (ColumnId: string, taskId: string, updatedTask: Task) => void;
+    deleteTask: (columnId: string, targetColumnId: string, taskId: string) => void;
+}
+
+
+export const TaskContext = createContext<TaskContextTpe | undefined>()undefined;
+
+const initialData : Column[] = [
         { id: 'todo', title: 'Todo', tasks: []},
         { id: 'inprogress', title: 'In Progress', tasks: []},
         { id: 'done', title: 'Done', tasks: []}
-    ],
-};
+];
 
-export const TasksProvider = ({ children }) => {
-    const [columns, setColumns] = useState(initialData.columns);
 
-    const addTask = (columnId, task) {
+interface TasksProviderProps  {
+    children: ReactNode;
+}
+
+export const TasksProvider: React.FC<TasksProviderProps> = ({ children }) => {
+    const [columns, setColumns] = useState<Column[]>(initialData);
+
+
+
+    const addTask = (columnId: string, task: Task) => {
         setcolumns(prev => prev.map(col =>
-            col.id === columnId? {...col, tasks: [...col.tasks,task] }
+            col.id === columnId? {...col, tasks: [...col.tasks,task] } : col
         ));
     };
 
-    const updateTask = ( columnId, taskId, updateTask) => {
-        setcolumns(ProgressEvent.map(col => {
+    const updateTask = ( columnId: string, taskId: string, updateTask: Task) => {
+        setcolumns(prev => prev.map(col => {
             if(col.id !== columnId) return col;
             return {
                 ...col,
                 tasks: col.tasks.map(t => t.id === taskId ? updateTask : t),
-            };
+        };
         }));
     };
 
-    const deleteTask = (columnId, taskId) => {
+    const deleteTask = (columnId: string, taskId: string) => {
         setColumns(prev => prev.map(col => {
             if (col.id !== columnId) return col;
             return {
@@ -39,13 +54,13 @@ export const TasksProvider = ({ children }) => {
         }));
     };
 
-    const moveTask = (sourceColId, targetColId, taskId) => {
+    const moveTask = (sourceColumn: string, targetColumnId: string, taskId: string) => {
         //hämta task från source
-        let taskToMove;
+        let taskToMove: Task | undefined;
         setColumns(prev => {
             const newCols = prev.map(col => {
-                if (col.id === sourceColId) {
-                    taskToMove = col.tasks.fins(t => t.id === taskId);
+                if (col.id === sourceColumnId) {
+                    taskToMove = col.tasks.find(t => t.id === taskId);
                     return {
                         ...col,
                         tasks: col.tasks.filer(t => t.id !== taskId),
@@ -55,7 +70,7 @@ export const TasksProvider = ({ children }) => {
             });
             //Lägg till i target
             return newCols.map(col => {
-                if (col.id === targetColId && taskToMove) {
+                if (col.id === targetColumnId && taskToMove) {
                     return {
                         ...col,
                         tasks: [...col.tasks, taskToMove],
