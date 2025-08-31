8945fae1a5d7598d8db71b41f0845e20f7060955
idasoderlund <soderlundida7711@gmail.com>
2025-09-01 01:22:02 +0200

    trying to fix bugging

--- src/Components/Column.tsx
+++ src/Components/Column.tsx
@@ -1,7 +1,7 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import Card from "./Card";
 import { useDrop } from "react-dnd/dist";
-import { TaskContext } from "./TaskContext";
+import { TaskContext } from "../Contexts/Context";
 import type { Column as ColumnType } from "../Types/Types";
 //import { Task } from "../Contexts/TaskContexts";
 
@@ -9,20 +9,32 @@ interface ColumnProps {
   column: ColumnType;
 }
 
+interface DropResult {
+  isOver: boolean;
+}
+
 const Column: React.FC<ColumnProps> = ({ column }) => {
   const ctx = useContext(TaskContext);
   if (!ctx) throw new Error("TaskContext saknas");
 
-  const [{ isOver }, drop] = useDrop({
+  const { moveTask } = ctx;
+
+  const divRef = useRef<HTMLDivElement>(null);
+
+  const [{ isOver }, drop] = useDrop<DropResult, unknown, { isOver: boolean }>({
     accept: "TASK",
-    drop: (item: { id: string; sourceColumnId: string }) => {
+    drop: (item: { id: string; sourceColumnId: string }, monitor) => {
       moveTask(item.sourceColumnId, column.id, item.id);
     },
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+    }),
   });
 
+  drop(divRef);
   return (
     <div
-      ref={drop}
+      ref={divRef}
       style={{
         backgroundColor: isOver ? "#f0f0f0" : "#fff",
         padding: "10px",
@@ -31,7 +43,7 @@ const Column: React.FC<ColumnProps> = ({ column }) => {
       }}
     >
       <h3>{column.title}</h3>
-      {column.tasks.map((task) => (
+      {column.tasks?.map((task) => (
         <Card key={task.id} task={task} columnId={column.id} />
       ))}
     </div>
